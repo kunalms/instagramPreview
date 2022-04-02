95da941fe330fc64be696df1bd5b5f09880a23fb
Kunal Sharma <kunal.sharma@firstinsight.com>
2022-04-02 11:29:58 +0530

    add user.service

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {InstagramPost} from "../../shared/models/instagram-post";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private readonly INSTAGRAM_POST_API = 'https://s3-ap-southeast-1.amazonaws.com/he-public-data/instaf913f18.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getPostsForUser(): Observable<InstagramPost>{
+    return this.httpClient.get<InstagramPost>(this.INSTAGRAM_POST_API);
+  }
+}
